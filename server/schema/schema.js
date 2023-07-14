4765ab2bc7c667f8ea00f1938a1511d39c8f0749
mdallen5393 <allen_matthew@outlook.com>
2023-07-14 13:00:01 -0500

    Task 0. GraphQL Schema - initialized express server and added TaskType schema

---
+++ server/schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  })
+});
