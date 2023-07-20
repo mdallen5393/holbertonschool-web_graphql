b2a58fe2927341e9c0a6199412214b1ddad99574
mdallen5393 <allen_matthew@outlook.com>
2023-07-20 11:54:47 -0500

    Task 13. Tasks details query

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 mongoose.connect('mongodb+srv://root:root@cluster0.br1451q.mongodb.net/?retryWrites=true&w=majority')
 
 mongoose.connection.once('open', () =>
-  console.log('connected to database')
+console.log('connected to database')
 );
 
 const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
